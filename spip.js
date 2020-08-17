ded528c5199607f77ea55f6cebf88badb9db6126
JamesRezo <james.hautot@sogeti.com>
2020-08-17 16:59:33 +0200

    Making the pie

--- spip.js
+++ spip.js
@@ -1,20 +1,25 @@
-async function getSpipJson()
+function getSpipJson()
 {
-  let data = await fetch('https://jamesrezo.github.io/stats-backup/spip.json')
+  fetch('https://jamesrezo.github.io/stats-backup/spip.json')
     .then(response => response.json())
-    .then(spip => populate("#spip-data", spip));
+    .then(spip => populate("#spip-data", spip))
+    .then(spip => doPie("#spip-pie", spip));
 }
 
-async function getPhpJson()
+function getPhpJson()
 {
-  let data = await fetch('https://jamesrezo.github.io/stats-backup/php.json')
+  fetch('https://jamesrezo.github.io/stats-backup/php.json')
     .then(response => response.json())
-    .then(php => populate("#php-data", php));
+    .then(php => populate("#php-data", php))
+    .then(php => doPie("#php-pie", php));
 }
 
 function populate(tableElement, data)
 {
   var myTable = document.querySelector(tableElement);
+  myTable.tBodies[0].removeChild(myTable.tBodies[0].firstElementChild);
+
+  let sumSites = 0;
   for (var i = 0; i < data.length; i++) {
     var version = document.createElement('td');
     version.textContent = data[i].version;
@@ -25,7 +30,22 @@ function populate(tableElement, data)
     raw.appendChild(sites);
 
     myTable.tBodies[0].appendChild(raw);
+    sumSites = sumSites + data[i].sites;
   }
+
+  myTable.tFoot.firstElementChild.cells[1].textContent = sumSites;
+
+  return data;
+}
+
+function doPie(svg, data)
+{
+  var myPie = document.querySelector(svg);
+  var pie = d3.pie(data);
+
+  console.log(data);
+
+  return data;
 }
 
 getSpipJson();
